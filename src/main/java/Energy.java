44ee55b5043012f267c43e897ddc4b45df0f8c2b
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-12-14 02:53:55 -0500

    refactored into 2 classes

---
+++ src/main/java/Energy.java
@@ -0,0 +1,79 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Energy {
+    private int[][] energyArray;
+    BufferedImage energy;
+    private int max;
+    private int min;
+
+
+    Energy(BufferedImage pic)
+    {
+        this.energyArray = buildEnergyArray(pic);
+        createEnergyPic(energyArray);
+    }
+
+    private void createEnergyPic(int[][] energyArray) {
+        this.energy = new BufferedImage(energyArray.length, energyArray[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int row = 0; row < energyArray.length; row++) {
+            for (int col = 0; col < energyArray[row].length; col++) {
+                int value = energyArray[row][col] == 390150 ? max : energyArray[row][col];
+                int valueb = (int)((value - min)*255.0) / (max - min); //max-min x work
+                valueb = Math.min(valueb, 255);
+                Color valuec =  new Color(valueb, valueb, valueb);
+                int brightness = valuec.getRGB();
+                energy.setRGB(row,col,brightness);
+            }
+        }
+
+        File outputFile = new File("saved.png");
+        try {
+            ImageIO.write(energy, "png", outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int[][] buildEnergyArray(BufferedImage original) {
+        this.max = 0;
+        this.min = 390150;
+        energyArray = new int[original.getWidth()][original.getHeight()];
+
+        for (int row = 0; row < energyArray.length; row++) {
+            for (int col = 0; col < energyArray[row].length; col++) {
+                int energyPixel;
+                if (row == 0 || row >= (original.getWidth()-1) || col == 0 || col >= (original.getHeight()-1)) {
+                    energyPixel = 390150;
+                } else {
+                    Color left = new Color(original.getRGB(row, col - 1));
+                    Color right = new Color(original.getRGB(row, col + 1));
+                    Color up = new Color(original.getRGB(row - 1, col));
+                    Color down = new Color(original.getRGB(row + 1, col));
+
+                    energyPixel = (up.getRed() - down.getRed()) * (up.getRed() - down.getRed())
+                            + (up.getGreen() - down.getGreen()) * (up.getGreen() - down.getGreen())
+                            + (up.getBlue() - down.getBlue()) * (up.getBlue() - down.getBlue())
+                            + (left.getRed() - right.getRed()) * (left.getRed() - right.getRed())
+                            + (left.getGreen() - right.getGreen()) * (left.getGreen() - right.getGreen())
+                            + (left.getBlue() - right.getBlue()) * (left.getBlue() - right.getBlue());
+
+                    if (energyPixel > max)
+                    {
+                        max = energyPixel;
+                    }
+
+                    if (energyPixel < min)
+                    {
+                        min = energyPixel;
+                    }
+                }
+                energyArray[row][col] = energyPixel;
+            }
+        }
+        return energyArray;
+    }
+}
--- src/main/java/SeamCarving.java
+++ src/main/java/SeamCarving.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SeamCarving {
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static java.lang.Math.max;
+
+public class SeamCarving {
+
+    public static void main(String[] args) {
+        try {
+
+            BufferedImage original = ImageIO.read(new File("C:\\Users\\mirdr\\IdeaProjects\\DreyfussSeamCarving\\src\\main\\resources\\Broadway_tower_edit.jpg"));
+
+            Energy energy = new Energy(original);
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            e.printStackTrace();
+        }
+    }
+
 }
