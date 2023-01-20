608b875b20f5e7b0aba50b6a64ed1ba2daa0ee4f
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2023-01-19 23:28:05 -0500

    test for calcRemoveSeam

--- src/test/java/SeamsTest.java
+++ src/test/java/SeamsTest.java
@@ -1,22 +1,30 @@
 import ImageData.Energy;
 import ImageData.Seams;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SeamsTest {
+    Energy testEnergy;
+    Seams testSeams;
 
-    @Test
-    void createHorizontalSeamArray() {
-        Energy testEnergy = Mockito.mock(Energy.class);
+    @BeforeEach
+    void Setup(){
+        this.testEnergy= Mockito.mock(Energy.class);
         double[][] testEnergyArray = {
                 {1.0,4.0,3.0,5.0},
                 {3.0,2.0,1.0,6.0},
                 {2.0,5.0,4.0,1.0},
                 {4.0,3.0,2.0,1.0}};
         Mockito.doReturn(testEnergyArray).when(testEnergy).getEnergyArray();
-        Seams testSeams = new Seams(testEnergy);
+        this.testSeams = new Seams(testEnergy);
+    }
+
+    @Test
+    void createHorizontalSeamArray() {
+
 
         double[][] expectHorizontalSeams = {
                 {1.0,4.0,3.0,5.0},
@@ -25,20 +33,11 @@ class SeamsTest {
                 {9.0,8.0,7.0,6.0}};
 
 
-        assertArrayEquals(expectHorizontalSeams,testSeams.getHorizontalSeamArray());
+        assertArrayEquals(expectHorizontalSeams, testSeams.getHorizontalSeamArray());
     }
 
     @Test
     void createVerticalSeamArray() {
-        Energy testEnergy = Mockito.mock(Energy.class);
-        double[][] testEnergyArray = {
-                {1.0,4.0,3.0,5.0},
-                {3.0,2.0,1.0,6.0},
-                {2.0,5.0,4.0,1.0},
-                {4.0,3.0,2.0,1.0}};
-        Mockito.doReturn(testEnergyArray).when(testEnergy).getEnergyArray();
-        Seams testSeams = new Seams(testEnergy);
-
         double[][] expectVerticalSeams = {
                 {1.0,3.0,2.0,4.0},
                 {5.0,3.0,7.0,5.0},
@@ -53,6 +52,14 @@ class SeamsTest {
     @Test
     void calcRemoveSeam() {
 
+        double[][] testRemove ={
+                {1.0,3.0,2.0,4.0},
+                {5.0,3.0,7.0,5.0},
+                {6.0,4.0,7.0,7.0},
+                {9.0,10.0,5.0,8.0}};
+        int[] expectRemove = {0,1,1,2};
+
+        assertArrayEquals(expectRemove, testSeams.calcRemoveSeam(testRemove));
     }
 
 }
